c87140820e86f20c1130499a16f20909bcb684be
victorduclos <victor.duclos@protonmail.com>
2022-04-10 21:50:39 +0200

    `onSubmit` test

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -1,5 +1,5 @@
 import { Input } from '../components/form/Input';
-import { Form, useForm } from 'graneet-form';
+import { Form, useForm, FieldValues } from 'graneet-form';
 import { MouseEventHandler } from "react";
 
 export const SimpleForm = () => {
@@ -9,8 +9,12 @@ export const SimpleForm = () => {
     console.log(form.getFormValues());
   }
 
+  const handleSubmit: (formValues: FieldValues) => void = (formValues: FieldValues) => {
+    console.log(formValues);
+  }
+
   return (
-    <Form form={form}>
+    <Form form={form} onSubmit={handleSubmit}>
       SimpleForm
       <Input name="firstName">
       </Input>
